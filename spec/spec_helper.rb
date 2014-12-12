f91b81d946a1d38fcd28886dd3f70ed545530c7e
cblanc <cablanchard@gmail.com>
2014-12-12 11:39:59 +0000

    Enable vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,20 @@
+# Enable VCR
+require 'vcr'
+
+VCR.configure do |c|
+	c.cassette_library_dir = 'spec/vcr_cassettes'
+	c.hook_into :webmock
+end
+
+RSpec.configure do |c|
+	c.around(:each) do |example|
+		VCR.use_cassette(example.metadata[:full_description]) do
+			example.run
+		end
+	end
+end
+
+# Configure Ideal Postcodes lib
 require 'ideal_postcodes'
+IdealPostcodes.base_url = 'https://localhost:1337'
+IdealPostcodes.api_key = "gandhi"
