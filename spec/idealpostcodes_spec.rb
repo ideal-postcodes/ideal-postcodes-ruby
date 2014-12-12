ff7ae192bbdc77b9e255ba83eb3d99b6971a62e4
cblanc <cablanchard@gmail.com>
2014-12-12 11:40:12 +0000

    Added test for #request

--- spec/idealpostcodes_spec.rb
+++ spec/idealpostcodes_spec.rb
@@ -2,11 +2,35 @@ require 'spec_helper'
 
 describe IdealPostcodes do
 	describe "#request" do
-		it "generates a HTTP request"
-		it "raises an error if no key is provided"
-		it "raises authentication error if invalid key is provided"
-		it "raises token exhausted error if key balance is depleted"
-		it "raises limit reached error if a limit has been breached"
-		it "raises 404 for invalid resource"
+		it "generates a HTTP request" do
+			response = IdealPostcodes.request :get, "postcodes/ID1 1QD"
+			expect(response[:code]).to eq(2000)
+		end
+		it "raises an error if no key is provided" do
+			old_key = IdealPostcodes.api_key
+			IdealPostcodes.api_key = nil
+			expect do
+				response = IdealPostcodes.request :get, "postcodes/ID1 1QD"
+			end.to raise_error(IdealPostcodes::AuthenticationError)
+			IdealPostcodes.api_key = old_key
+		end
+		it "raises authentication error if invalid key is provided" do
+			old_key = IdealPostcodes.api_key
+			IdealPostcodes.api_key = "foo"
+			expect do
+				response = IdealPostcodes.request :get, "postcodes/ID1 1QD"
+			end.to raise_error(IdealPostcodes::AuthenticationError)
+			IdealPostcodes.api_key = old_key
+		end
+		it "raises token exhausted error if key balance is depleted" do
+			expect do
+				response = IdealPostcodes.request :get, "postcodes/ID1 CLIP"
+			end.to raise_error(IdealPostcodes::TokenExhaustedError)
+		end
+		it "raises limit reached error if a limit has been breached" do
+			expect do
+				response = IdealPostcodes.request :get, "postcodes/ID1 CHOP"
+			end.to raise_error(IdealPostcodes::LimitReachedError)
+		end
 	end
 end
