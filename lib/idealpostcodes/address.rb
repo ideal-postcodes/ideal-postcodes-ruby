6b762df794ab9ab1110b48098da8121ec6e16267
Christopher Blanchard <git@nf3.co>
2021-11-17 15:38:06 +0000

    Prettier

--- lib/idealpostcodes/address.rb
+++ lib/idealpostcodes/address.rb
@@ -1,31 +1,31 @@
 module IdealPostcodes
-	module Address
-		class SearchResult
-			attr_reader :page, :limit, :addresses
-			def initialize response
-				@page = response[:result][:page]
-				@limit = response[:result][:limit]
-				@addresses = response[:result][:hits]
-			end
-		end
+  module Address
+    class SearchResult
+      attr_reader :page, :limit, :addresses
+      def initialize(response)
+        @page = response[:result][:page]
+        @limit = response[:result][:limit]
+        @addresses = response[:result][:hits]
+      end
+    end
 
-		def self.lookup udprn
-			begin
-				response = IdealPostcodes.request :get, "addresses/#{udprn}"
-				address = response[:result]
-			rescue IdealPostcodes::IdealPostcodesError => error
-				raise error unless error.response_code == 4044
-				address = nil
-			end
-			address
-		end
+    def self.lookup(udprn)
+      begin
+        response = IdealPostcodes.request :get, "addresses/#{udprn}"
+        address = response[:result]
+      rescue IdealPostcodes::IdealPostcodesError => error
+        raise error unless error.response_code == 4044
+        address = nil
+      end
+      address
+    end
 
-		def self.search search_term, options = {}
-			query = { query: search_term }
-			query[:limit] = options[:limit] unless options[:limit].nil?
-			query[:page] = options[:page] unless options[:page].nil?
-			response = IdealPostcodes.request :get, "addresses", query
-			SearchResult.new response
-		end
-	end
+    def self.search(search_term, options = {})
+      query = { query: search_term }
+      query[:limit] = options[:limit] unless options[:limit].nil?
+      query[:page] = options[:page] unless options[:page].nil?
+      response = IdealPostcodes.request :get, 'addresses', query
+      SearchResult.new response
+    end
+  end
 end
--- lib/idealpostcodes/util.rb
+++ lib/idealpostcodes/util.rb
@@ -1,29 +1,34 @@
 module IdealPostcodes
-	class Util
+  class Util
+    def self.merge_params(hash)
+      result = []
+      hash.each do |key, value|
+        result << "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
+      end
+      result.join('&')
+    end
 
-		def self.merge_params(hash)
-			result = []
-			hash.each do |key, value|
-				result << "#{CGI.escape(key.to_s)}=#{CGI.escape(value.to_s)}"
-			end
-			result.join('&')
-		end
-
-		def self.keys_to_sym(object)
-			case object
-			when Hash
-				temp = {}
-				object.each do |key, value|
-					key = (key.to_sym rescue key) || key
+    def self.keys_to_sym(object)
+      case object
+      when Hash
+        temp = {}
+        object.each do |key, value|
+          key =
+            (
+              begin
+                key.to_sym
+              rescue StandardError
+                key
+              end
+            ) || key
           temp[key] = keys_to_sym(value)
-				end
-				temp
-			when Array
-				object.map { |elem| keys_to_sym(elem) }
-			else
-				object
-			end
-		end
-
-	end
+        end
+        temp
+      when Array
+        object.map { |elem| keys_to_sym(elem) }
+      else
+        object
+      end
+    end
+  end
 end
